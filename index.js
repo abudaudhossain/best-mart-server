c75bd3ca4e3ee8040942f2681fb308a2ed59d9d8
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-12-21 21:35:32 +0600

    add products api

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqqvk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("connection success...")
+    const database = client.db("BestMart+");
+    const productsCollection = database.collection('products');
+    // const bookingCollection = database.collection('')
+    //get all products api
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find({}).toArray();
+
+      res.send(result)
+    }
+
+
+    )
+
+  } finally {
+
+  }
+}
+run().catch(console.dir())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Best Mart+')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Best mart server${port}`)
 })
